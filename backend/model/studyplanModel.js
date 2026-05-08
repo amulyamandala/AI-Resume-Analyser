bd40756a2955c9696ea55faf1fb29d2cd05c83c3
amulyamandala <amulyamandala007@gmail.com>
2026-05-08 10:16:53 +0530

    backend

---
+++ backend/model/studyplanModel.js
@@ -0,0 +1,39 @@
+import { Schema,model,Types } from "mongoose";
+const studyPlanSchema = new Schema(
+  {
+    userId: {
+      type: Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+
+    analysisId: {
+      type: Types.ObjectId,
+      ref: "analysis",
+      required: true,
+    },
+
+    weakTopics: [String],
+
+    generatedBy: {
+      type: String,
+      default: "gemini",
+    },
+
+    roadmap: [
+      {
+        week: Number,
+
+        title: String,
+
+        tasks: [String],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    strict: "throw",
+  }
+);
+export const StudyPlan = model("studyPlan", studyPlanSchema);
